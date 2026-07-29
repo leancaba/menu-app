fcf99767b02ddcc18c90bdced5af1c2d06e72a50
leancaba <leadelciclon@gmail.com>
2026-07-29 17:26:53 -0300

    Update route.js

--- app/api/menu/route.js
+++ app/api/menu/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server';
 import { getMenu, saveMenu } from '@/lib/store';
+export const dynamic = 'force-dynamic';
 
 export async function GET() {
   const menu = await getMenu();
